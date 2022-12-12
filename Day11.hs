c1a7a40f2e83c8a0427377cbb26b881f5f22f10c
Austin Platt <austinplatt@gmail.com>
2022-12-12 00:17:02 +0000

    solve day 12 part ii

--- Day11.hs
+++ Day11.hs
@@ -6,7 +6,7 @@
 module Day11 (solution) where
 
 import AOC (Parser, Solution (..), forceMaybe, head', last', parseInt, sepByNewline, tail')
-import Control.Lens ((%~), (^.), _Just)
+import Control.Lens (Field1 (_1), view, (%~), (.~), (^.), _Just)
 import Control.Lens.At (At (..))
 import Control.Lens.Operators ((?~))
 import Data.Generics.Labels
@@ -14,7 +14,6 @@ import qualified Data.Map.Strict as Map
 import Relude hiding (Op)
 import Text.Megaparsec (sepBy, sepBy1)
 import Text.Megaparsec.Char (char, newline, string)
-import Text.Megaparsec.Debug (dbg)
 
 data Var = Old | Lit Int deriving (Show)
 
@@ -77,10 +76,9 @@ parseMonkey = do
 parseInput :: Parser Input
 parseInput = Map.fromList <$> sepByNewline parseMonkey
 
-extractItem :: Monkey -> (Maybe Int, Monkey)
-extractItem m = case m ^. #items of
-  [] -> (Nothing, m)
-  (i : is) -> (Just i, m & #items %~ tail')
+extractItems :: Map.Map Int Monkey -> Int -> ([Int], Map.Map Int Monkey)
+extractItems ms i = case ms ^. (at i . _Just . #items) of
+  items -> (items, ms & (at i . _Just . #items) .~ [])
 
 div3 :: Int -> Int
 div3 n = n `div` 3
@@ -99,36 +97,45 @@ runOp (Operation v1 op v2) n = f n1 n2
 
 runTest (d, m1, m2) n = if n `mod` d == 0 then m1 else m2
 
-runItem :: Monkey -> Int -> (Int, Int)
-runItem m item = (i, w3)
+runItem :: (Int -> Int) -> Monkey -> Int -> (Int, Int)
+runItem f m item = (i, w3)
   where
     w1 = item
     w2 = runOp (m ^. #operation) w1
-    w3 = div3 w2
+    w3 = f w2
     i = runTest (m ^. #test) w3
 
-doMonkey :: Map Int Monkey -> Int -> Map Int Monkey
-doMonkey ms i = ms''
+doMonkey f ms i = ms''
   where
     m = forceMaybe $ ms ^. at i
-    (maybeItem, m') = extractItem m
-    ms' = ms & at i ?~ m'
-    go Nothing = ms'
-    go (Just item) = doMonkey ns i
+    (items, ms') = extractItems ms i
+    go :: Map.Map Int Monkey -> Int -> Map.Map Int Monkey
+    go ns item = ns'
       where
-        (nextMonkey, nextItem) = runItem m item
-        ns =
-          ms'
+        (nextMonkey, nextItem) = runItem f m item
+        ns' =
+          ns
             & (at nextMonkey . _Just . #items) %~ (\items -> items ++ [nextItem])
             & (at i . _Just . #inspections) %~ (+ 1)
-    ms'' = go maybeItem
+    ms'' = foldl' go ms' items
 
-doRound :: Map Int Monkey -> Map Int Monkey
-doRound ms = foldl' doMonkey ms [0 .. 7]
+doRound f ms = foldl' (doMonkey f) ms [0 .. 7]
 
-solve1 = product . take 2 . reverse . sort . map (\m -> m ^. #inspections) . Map.elems . head' . drop 20 . iterate doRound
+solve f n = product . take 2 . reverse . sort . map (\m -> m ^. #inspections) . Map.elems . head' . drop n . uncurry iterate . (doRound . f &&& id)
 
-solve2 _ = Just 2
+solve1 = solve f 20
+  where
+    f ms = div3
+
+divisors = map (view (#test . _1)) . Map.elems
+
+-- use the chinese remainder theorem to reduce n to something smaller which
+-- keeps the mod values constant
+makeModAll ms = \n -> n `mod` k
+  where
+    k = product $ divisors ms
+
+solve2 = solve makeModAll 10000
 
 solution =
   Solution
